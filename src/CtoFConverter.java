24ff1af36fcc6f98f1e3fb8fa073b38ed62bc3cb
UCscottg8 <134948683+UCscottg8@users.noreply.github.com>
2023-06-14 21:48:10 -0400

    initial

---
+++ src/CtoFConverter.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CtoFConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Double temperatureCelsius;
+
+        do {
+            System.out.println("Please enter a temperature in Celsius:");
+            while (!scanner.hasNextDouble()) {
+                System.out.println("That's not a valid number! Please enter a number:");
+                scanner.next();
+            }
+            temperatureCelsius = scanner.nextDouble();
+        } while (temperatureCelsius == null);
+
+        Double temperatureFahrenheit = temperatureCelsius * 9/5 + 32;
+        System.out.println("The equivalent temperature in Fahrenheit is: " + temperatureFahrenheit);
+    }
+}
