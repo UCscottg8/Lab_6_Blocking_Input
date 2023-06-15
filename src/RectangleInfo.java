737486d13196f364d4cd8a48995096ddc0b85e58
UCscottg8 <134948683+UCscottg8@users.noreply.github.com>
2023-06-14 22:36:45 -0400

    ver 1

--- src/RectangleInfo.java
+++ src/RectangleInfo.java
@@ -1,37 +1,32 @@
 import java.util.Scanner;
 
-public class FuelCosts {
+public class RectangleInfo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double numberOfGallons, fuelEfficiency, pricePerGallon;
+        double width, height;
 
         do {
-            System.out.println("Enter the number of gallons of gas in the tank:");
+            System.out.println("Please enter the width of the rectangle:");
             while (!scanner.hasNextDouble()) {
-                System.out.println("Invalid input. Enter a number:");
+                System.out.println("Invalid input. Please enter a number:");
                 scanner.next();
             }
-            numberOfGallons = scanner.nextDouble();
+            width = scanner.nextDouble();
 
-            System.out.println("Enter the fuel efficiency in miles per gallon:");
+            System.out.println("Please enter the height of the rectangle:");
             while (!scanner.hasNextDouble()) {
-                System.out.println("Invalid input. Enter a number:");
+                System.out.println("Invalid input. Please enter a number:");
                 scanner.next();
             }
-            fuelEfficiency = scanner.nextDouble();
-            System.out.println("Enter the price of gas per gallon:");
-            while (!scanner.hasNextDouble()) {
-                System.out.println("Invalid input. Enter a number:");
-                scanner.next();
-            }
-            pricePerGallon = scanner.nextDouble();
-        } while (numberOfGallons <= 0 || fuelEfficiency <= 0 || pricePerGallon <= 0);
+            height = scanner.nextDouble();
+        } while (width <= 0 || height <= 0);
 
-        double costPerHundredMiles = (100 / fuelEfficiency) * pricePerGallon;
-        double distanceCarCanGo = numberOfGallons * fuelEfficiency;
+        double area = width * height;
+        double perimeter = 2 * (width + height);
+        double diagonal = Math.sqrt(width * width + height * height);
 
-        //output statements
-        System.out.printf("The cost per 100 miles is: $%.2f%n", costPerHundredMiles);
-        System.out.printf("The car can go %.2f miles with the gas in the tank.%n", distanceCarCanGo);
+        System.out.printf("The area of the rectangle is: %.2f%n", area);
+        System.out.printf("The perimeter of the rectangle is: %.2f%n", perimeter);
+        System.out.printf("The length of the diagonal of the rectangle is: %.2f%n", diagonal);
     }
 }
